ae2e91a03722760018263ac6c11814af035d37ed
DAVIDE <davideliuzzo189@gmail.com>
2025-02-09 14:46:34 +0100

    modifiche

--- public/index.js
+++ public/index.js
@@ -1,36 +1,27 @@
-const inputFile = document.querySelector('#file');
-const button = document.querySelector("#button");
-const link = document.querySelector("#link");
+(async () => {
+    const inputFile = document.querySelector('#file');
+    const button = document.querySelector("#button");
+    const link = document.querySelector("#link");
+    const inputDescription = document.getElementById("inputDescription");
 
-const render = async () => {
-    const list = await fetch("/filelist");
-    const data = await list.json();
-
-    const html = data
-        .map(e => `<li><a href="${e}">${e}</a></li>`)
-        .join("");
-
-    link.innerHTML = `<ul>${html}</ul>`;
-};
-
-
-await render();
-
-const handleSubmit = async (event) => {
-    const formData = new FormData();
-    formData.append("file", inputFile.files[0]);
-    const body = formData;
-    const fetchOptions = {
-        method: 'post',
-        body: body
-    };
-    try {
-        const res = await fetch("/upload", fetchOptions);
-        const data = await res.json();
-        await render();
-    } catch (e) {
-        console.log(e);
+    handleSubmit = async (event) => {
+        const formData = new FormData();
+        formData.append("file", inputFile.files[0]);
+        const body = formData;
+        body.description = inputDescription.value;
+        const fetchOptions = {
+            method: 'post',
+            body: body
+        };
+        try {
+            const res = await fetch("/upload", fetchOptions);
+            const data = await res.json();
+            link.setAttribute("href", data.url);
+            link.innerText = data.url;
+        } catch (e) {
+            console.log(e);
+        }
     }
-}
 
-button.onclick = handleSubmit;
+    button.onclick = handleSubmit;
+})();
